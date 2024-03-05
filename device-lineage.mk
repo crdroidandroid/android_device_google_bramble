3ce12ba5c2ae8368bcbbcbeda6bd841fc1e63add
306bobby <143777624+306bobby-android@users.noreply.github.com>
2024-03-05 00:03:39 -0500

    Remove HBMsvManager

--- device-lineage.mk
+++ device-lineage.mk
@@ -9,6 +9,3 @@ $(call inherit-product, device/google/redbull/device-lineage.mk)
 # CHRE
 $(call soong_config_set,chre,chre_daemon_dsp_library,//vendor/google/bramble:libadsprpc)
 
-# HBM
-PRODUCT_PACKAGES += \
-    HbmSVManagerOverlay
