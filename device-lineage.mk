c7a553fff7edce89206c2a1e796c84ea466007fe
306bobby-android <143777624+306bobby-android@users.noreply.github.com>
2024-04-07 15:57:32 -0400

    fixup: commit Change-Id: I5930149fe3df6058ef2e24cdfacc2106940588ac

---
+++ device-lineage.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/google/redbull/device-lineage.mk)
+
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.device-V2-ndk.vendor:64 \
+    android.hardware.camera.provider-V2-ndk.vendor:64
